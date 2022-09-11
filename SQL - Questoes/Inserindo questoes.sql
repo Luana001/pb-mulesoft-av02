de21281b1f326cea5fb297592a85b9a4d4c28ba3
Luana <lupinheiro.77@hotmail.com>
2022-09-11 15:46:58 -0300

    Adicionando script SQL

---
+++ SQL - Questoes/Inserindo questoes.sql
@@ -0,0 +1,18 @@
+
+INSERT INTO QUESTOES (pergunta, verdadeira, ativa) VALUES 
+("A nossa sede fica em São Paulo?", 1, 1);
+
+INSERT INTO QUESTOES (pergunta, verdadeira, ativa) VALUES 
+("A empressa foi criada em 1953?", 1, 1);
+
+INSERT INTO QUESTOES (pergunta,verdadeira, ativa) VALUES 
+("Os nossos primeiros produtos foram as barras de chocolate?", 0, 1);
+
+INSERT INTO QUESTOES (pergunta, verdadeira, ativa) VALUES 
+("A nossa empresa é famosa por bombons deliciosos?", 1, 1);
+
+INSERT INTO QUESTOES (pergunta, verdadeira, ativa) VALUES 
+("Nossa empresa fabrica apenas barras de chocolate?", 0, 1);
+
+INSERT INTO QUESTOES (pergunta, verdadeira, ativa) VALUES 
+("Temos filiais ativas por todo o país?", 1, 1);
